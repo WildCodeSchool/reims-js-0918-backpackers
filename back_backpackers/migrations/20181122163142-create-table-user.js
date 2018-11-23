e85f454a6bac43025f7981a079fde5019654b530
Michaël BOURMEAU <michael.bourmeau@outlook.fr>
2018-11-23 10:38:03 +0100

    migration table user

--- back_backpackers/migrations/20181122163142-create-table-user.js
+++ back_backpackers/migrations/20181122163142-create-table-user.js
@@ -1,27 +1,40 @@
-'use strict';
+"use strict";
 
 var dbm;
 var type;
 var seed;
 
 /**
-  * We receive the dbmigrate dependency from dbmigrate initially.
-  * This enables us to not have to rely on NODE_PATH.
-  */
+ * We receive the dbmigrate dependency from dbmigrate initially.
+ * This enables us to not have to rely on NODE_PATH.
+ */
 exports.setup = function(options, seedLink) {
   dbm = options.dbmigrate;
   type = dbm.dataType;
   seed = seedLink;
 };
 
-exports.up = function(db) {
-  return null;
+exports.up = function(db, callback) {
+  db.createTable(
+    "places",
+    {
+      idPlace: { type: "int", primaryKey: true, autoIncrement: true },
+      name: { type: "string", length: 255, notNull: true },
+      adress: { type: "text", notNull: true },
+      price: { type: "decimal", length: 50 },
+      type: { type: "string", length: 100, notNull: true },
+      capacity: { type: "int", length: 50 },
+      description: { type: "text" },
+      contact: { type: "text" }
+    },
+    callback
+  );
 };
 
-exports.down = function(db) {
-  return null;
+exports.down = function(db, callback) {
+  db.dropTable("places", callback);
 };
 
 exports._meta = {
-  "version": 1
+  version: 1
 };
