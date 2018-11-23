20924c97a1a6db41694c10273621844dc56e95db
Michel <descotesmichel@aol.com>
2018-11-23 12:48:55 +0100

    modif SinglePage

---
+++ front_backpackers/src/SinglePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button, ButtonGroup } from 'reactstrap';
+
+export default class Example extends React.Component {
+  render() {
+    return (
+        <ButtonGroup>
+        <Button>Left</Button>
+        <Button>Right</Button>
+      </ButtonGroup>
+    );
+  }
+}
