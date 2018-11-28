bfe4e39722653b87683fe440589f77bc204e12a0
Florentin C <florentin.coyard@gmail.com>
2018-11-28 15:52:17 +0100

    Created the migration that will insert data into activities table

---
+++ back_backpackers/migrations/20181128143033-insert-table-activities.js
@@ -0,0 +1,59 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function (options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function (db, callback) {
+  db.insert(
+    "activities",
+    [
+      "name", "creator", "price", "capacity", "description", "contact", "date"
+
+    ],
+    [
+      "Visite de la Tokyo Tower",
+      1,
+      7,
+      5,
+      "Cherche petit groupe pour visiter ensemble la Tokyo Tower ce Samedi !",
+      "0612345678",
+      "2019-02-09"
+    ],
+    callback
+  );
+  db.insert(
+    "activities",
+    [
+      "name", "creator", "capacity", "description", "contact", "date"
+
+    ],
+    [
+      "Tour du musée de Ueno",
+      1,
+      3,
+      "Je cherche des gens pour apprécier la culture jap au musée de Ueno !",
+      "0612345678",
+      "2019-02-20"
+    ],
+    callback
+  );
+};
+
+exports.down = function (db) {
+  return null;
+};
+
+exports._meta = {
+  "version": 1
+};
