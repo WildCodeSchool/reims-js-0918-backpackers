b40db288cabec8b1b1048e6b9d64b7ee4af3e9ae
CharlesC51 <c.crepin@net-c.fr>
2018-12-04 10:56:00 +0100

    test action ok

--- front_backpackers/src/actions/actions.js
+++ front_backpackers/src/actions/actions.js
@@ -1,6 +1,15 @@
-import { VIEW_PLACES } from "./actionTypes";
+import { VIEW_PLACES, VIEW_ACTIVITIES, FETCH_ACTIVITIES } from "./actionTypes";
 
 export const makeViewPlacesAction = places => ({
   type: VIEW_PLACES,
   places
+});
+
+export const makeViewActivitiesAction = activities => ({
+  type: VIEW_ACTIVITIES,
+  activities
+});
+
+export const makeFetchActivitiesAction = () => ({
+  type: FETCH_ACTIVITIES
 });
