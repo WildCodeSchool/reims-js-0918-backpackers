3d772f0a1b92e76f01c629236fe3353acb05a08a
Michaël BOURMEU <michael.bourmeau@outlook.fr>
2018-12-05 17:29:07 +0100

    css and Nav balise

--- front_backpackers/src/HomePage/Sidebar.js
+++ front_backpackers/src/HomePage/Sidebar.js
@@ -1,59 +1,73 @@
 import React, { Fragment } from "react";
-import { Row, Col } from "reactstrap";
+import { Nav, NavItem, NavLink, Navbar } from "reactstrap";
 
-import Michel from "../img/Michel400.jpg";
+import Michel from "../images/Michel400.jpg";
 import "./Sidebar.css";
 
 const Sidebar = ({ backdropClickHandler, show }) => (
   <Fragment>
-    <nav className={show ? "side-drawer open" : "side-drawer"}>
-      <div className="profil mt-4">
+    <Navbar className={show ? "side-drawer open" : "side-drawer"}>
+      <NavItem className="profil d-flex flex-column align-items-center mt-4">
         <img src={Michel} alt="profil" />
         <h4>Michel Descotes</h4>
-        <p className="m-0">michel.descotes@mail.com</p>
-      </div>
-      <Row>
-        <Col xs={{ size: 8, offset: 2 }} className="homeUnderline mt-1 mb-4" />
-      </Row>
-      <ul className="menu">
-        <li>
+        <p className="rounded-circle">michel.descotes@mail.com</p>
+      </NavItem>
+      <NavItem>
+        <div className="underline mt-3 mb-4" />
+      </NavItem>
+      <Nav className="menu d-flex flex-column align-items-baseline align-self-baseline">
+        <NavItem>
           <i className="mr-2 fas fa-user-alt" />
-          <a href="/">Profil</a>
-        </li>
-        <li>
+          <NavLink className="pl-0" href="/">
+            Profil
+          </NavLink>
+        </NavItem>
+        <NavItem>
           <i className="mr-2 far fa-comments" />
-          <a href="/">Messages</a>
-        </li>
-        <li>
+          <NavLink className="pl-0" href="/">
+            Messages
+          </NavLink>
+        </NavItem>
+        <NavItem>
           <i className="mr-2 far fa-calendar-alt" />
-          <a href="/">Calendrier</a>
-        </li>
-        <li>
+          <NavLink className="pl-0" href="/">
+            Calendrier
+          </NavLink>
+        </NavItem>
+        <NavItem>
           <i className="mr-2 far fa-heart" />
-          <a href="/">Coups de coeur</a>
-        </li>
-        <li>
+          <NavLink className="pl-0" href="/">
+            Coups de coeur
+          </NavLink>
+        </NavItem>
+        <NavItem>
           <i className="mr-2 fas fa-wrench" />
-          <a href="/">Paramètres</a>
-        </li>
-        <li>
+          <NavLink className="pl-0" href="/">
+            Paramètres
+          </NavLink>
+        </NavItem>
+        <NavItem>
           <i className="mr-2 fas fa-comment-alt" />
-          <a href="/">Demandes</a>
-        </li>
-        <li>
+          <NavLink className="pl-0" href="/">
+            Demandes
+          </NavLink>
+        </NavItem>
+        <NavItem>
           <i className="mr-2 fas fa-sign-out-alt" />
-          <a href="/">Se déconnecter</a>
-        </li>
-      </ul>
-      <ul className="mentions">
-        <li>
-          <a href="/">Mentions légales</a>
-        </li>
-        <li>
-          <a href="/">CGU</a>
-        </li>
-      </ul>
-    </nav>
+          <NavLink className="pl-0" href="/">
+            Se déconnecter
+          </NavLink>
+        </NavItem>
+      </Nav>
+      <Nav className="d-flex flex-column align-items-center">
+        <NavItem>
+          <NavLink href="/">Mentions légales</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="/">CGU</NavLink>
+        </NavItem>
+      </Nav>
+    </Navbar>
     <div
       onClick={() => backdropClickHandler()}
       className={show ? "backdrop" : ""}
