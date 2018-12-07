acba03a77bf3c432524f8fc6882f3c0fbf70885a
CharlesC51 <c.crepin@net-c.fr>
2018-12-07 11:19:27 +0100

    router publication to homepage

--- front_backpackers/src/FormPage/ActivityForm.js
+++ front_backpackers/src/FormPage/ActivityForm.js
@@ -1,20 +1,21 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Row, Col } from "reactstrap";
 import { Field, reduxForm } from "redux-form";
+import { Link } from "react-router-dom";
 import "./FormPage.css";
 
 const ActivityForm = props => {
   const { handleSubmit } = props;
   return (
-    <form onSubmit={handleSubmit}>
+    <Fragment>
       <Row className="header">
         <Col xs="2">
-          <p className="price text-primary">
-            <i className="fas fa-chevron-left" />
-          </p>
+          <Link to="/" className="price text-primary">
+            <i className="fas fa-chevron-left text-white" />
+          </Link>
         </Col>
         <Col xs="8">
-          <p className="text-center">Publier une annonce</p>
+          <p className="text-center mb-0">Publier une annonce</p>
         </Col>
       </Row>
       <h5 className="text-center publicationUnderline">Votre annonce</h5>
@@ -33,81 +34,84 @@ const ActivityForm = props => {
             <option>Restauration</option>
           </Field>
         </div> */}
-        <div>
-          <label htmlFor="name" />
-          <Field
-            id="name"
-            name="name"
-            component="input"
-            type="text"
-            placeholder="Titre"
-            className="field w-100 mt-2"
-          />
-        </div>
-        <div>
-          <label htmlFor="id_creator" />
-          <Field
-            id="id_creator"
-            name="id_creator"
-            component="input"
-            type="text"
-            placeholder="Créateur"
-            className="field w-100 mt-2"
-          />
-        </div>
-        <div>
-          <label htmlFor="id_place" />
-          <Field
-            id="id_place"
-            name="id_place"
-            component="input"
-            type="text"
-            placeholder="Lieu"
-            className="field w-100 mt-2"
-          />
-        </div>
-        <div>
-          <label htmlFor="description" />
-          <Field
-            id="decription"
-            name="description"
-            component="textarea"
-            type="text"
-            placeholder="Description"
-            className="field w-100 mt-2"
-          />
-        </div>
-        <div>
-          <label htmlFor="capacity" />
-          <Field
-            id="capacity"
-            name="capacity"
-            component="input"
-            type="text"
-            placeholder="participants"
-            className="field width mt-2"
-          />
-        </div>
-        <div>
-          <Field
-            id="price"
-            type="text"
-            name="price"
-            component="input"
-            placeholder="Prix"
-            className="field width mt-2"
-          />
-          <label htmlFor="price">€</label>
-        </div>
-        {/* <div>  DON'T TOUCH ! ! ! ! ! ! ! ! !
+
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="name" />
+            <Field
+              id="name"
+              name="name"
+              component="input"
+              type="text"
+              placeholder="Titre"
+              className="field w-100 mt-2"
+            />
+          </div>
+          <div>
+            <label htmlFor="id_creator" />
+            <Field
+              id="id_creator"
+              name="id_creator"
+              component="input"
+              type="text"
+              placeholder="Créateur"
+              className="field w-100 mt-2"
+            />
+          </div>
+          <div>
+            <label htmlFor="id_place" />
+            <Field
+              id="id_place"
+              name="id_place"
+              component="input"
+              type="text"
+              placeholder="Lieu"
+              className="field w-100 mt-2"
+            />
+          </div>
+          <div>
+            <label htmlFor="description" />
+            <Field
+              id="decription"
+              name="description"
+              component="textarea"
+              type="text"
+              placeholder="Description"
+              className="field w-100 mt-2"
+            />
+          </div>
+          <div>
+            <label htmlFor="capacity" />
+            <Field
+              id="capacity"
+              name="capacity"
+              component="input"
+              type="text"
+              placeholder="participants"
+              className="field width mt-2"
+            />
+          </div>
+          <div>
+            <Field
+              id="price"
+              type="text"
+              name="price"
+              component="input"
+              placeholder="Prix"
+              className="field width mt-2"
+            />
+            <label htmlFor="price">€</label>
+          </div>
+          {/* <div>  DON'T TOUCH ! ! ! ! ! ! ! ! !
           <label htmlFor="photos">Photos</label>
           <Field type="file" name="file" id="File" component="input" />
         </div> */}
-        <button type="submit" className="header cursor">
-          Publier l'annonce
-        </button>
+          <button type="submit" className="header cursor">
+            Publier l'annonce
+          </button>
+        </form>
       </div>
-    </form>
+    </Fragment>
   );
 };
 
