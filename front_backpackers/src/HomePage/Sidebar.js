c7c138c58ee593aee81be738cf4cfdfb32557187
CharlesC51 <c.crepin@net-c.fr>
2019-01-04 15:33:20 +0100

    start mapPlace

--- front_backpackers/src/HomePage/Sidebar.js
+++ front_backpackers/src/HomePage/Sidebar.js
@@ -4,44 +4,84 @@ import { NavLink } from "react-router-dom";
 
 import "./Sidebar.scss";
 
-const Sidebar = ({ backdropClickHandler, show, picture, firstName, lastName, mail }) => (
+const Sidebar = ({
+  backdropClickHandler,
+  show,
+  picture,
+  firstName,
+  lastName,
+  mail
+}) => (
   <Fragment>
-    <Navbar className={show ? "d-flex flex-column side-drawer open" : "d-flex flex-column side-drawer"}>
+    <Navbar
+      className={
+        show
+          ? "d-flex flex-column side-drawer open"
+          : "d-flex flex-column side-drawer"
+      }
+    >
       {firstName ? (
         <div className="profil d-flex flex-column align-items-center mt-3">
           <img src={picture} className="rounded-circle" alt="profil" />
           <h4>{`${firstName} ${lastName}`}</h4>
           <p>{mail}</p>
-        </div>) : ""}
-      < div className="menu d-flex flex-column align-items-baseline align-self-baseline pt-3">
+        </div>
+      ) : (
+        ""
+      )}
+      <div className="menu d-flex flex-column align-items-baseline align-self-baseline pt-3">
         <NavItem>
-          <NavLink to="/profil" className="pl-0 my-1"><i className="mr-2 fas fa-user-alt" />Profil</NavLink>
+          <NavLink to="/profil" className="pl-0 my-1">
+            <i className="mr-2 fas fa-user-alt" />
+            Profil
+          </NavLink>
         </NavItem>
         <NavItem>
-          <NavLink to="/" className="pl-0 my-1"> <i className="mr-2 far fa-comments" />Messages</NavLink>
+          <NavLink to="/" className="pl-0 my-1">
+            {" "}
+            <i className="mr-2 far fa-comments" />
+            Messages
+          </NavLink>
         </NavItem>
         <NavItem>
-          <NavLink to="/" className="pl-0 my-1"> <i className="mr-2 far fa-calendar-alt" />Calendrier</NavLink>
+          <NavLink to="/" className="pl-0 my-1">
+            {" "}
+            <i className="mr-2 far fa-calendar-alt" />
+            Calendrier
+          </NavLink>
         </NavItem>
         <NavItem>
-          <NavLink to="/" className="pl-0 my-1"> <i className="mr-2 far fa-heart" />Coups de coeur</NavLink>
+          <NavLink to="/" className="pl-0 my-1">
+            {" "}
+            <i className="mr-2 far fa-heart" />
+            Coups de coeur
+          </NavLink>
         </NavItem>
         <NavItem>
-          <NavLink to="/" className="pl-0 my-1"><i className="mr-2 fas fa-wrench" />Paramètres</NavLink>
+          <NavLink to="/" className="pl-0 my-1">
+            <i className="mr-2 fas fa-wrench" />
+            Paramètres
+          </NavLink>
         </NavItem>
         <NavItem>
-          <NavLink to="/" className="pl-0 my-1"><i className="mr-2 fas fa-comment-alt" />Demandes</NavLink>
+          <NavLink to="/" className="pl-0 my-1">
+            <i className="mr-2 fas fa-comment-alt" />
+            Demandes
+          </NavLink>
         </NavItem>
         <NavItem>
-          <NavLink to="/" className="pl-0 my-1"><i className="mr-2 fas fa-sign-out-alt" />Se déconnecter</NavLink>
+          <NavLink to="/login" className="pl-0 my-1">
+            <i className="mr-2 fas fa-sign-out-alt" />
+            Se déconnecter
+          </NavLink>
         </NavItem>
       </div>
       <div className="d-flex mb-2 flex-column mt-auto align-items-center">
         <NavItem>
-          <NavLink to="/" >Mentions légales</NavLink>
+          <NavLink to="/">Mentions légales</NavLink>
         </NavItem>
         <NavItem>
-          <NavLink to="/" >CGU</NavLink>
+          <NavLink to="/">CGU</NavLink>
         </NavItem>
       </div>
     </Navbar>
@@ -49,7 +89,7 @@ const Sidebar = ({ backdropClickHandler, show, picture, firstName, lastName, mai
       onClick={() => backdropClickHandler()}
       className={show ? "backdrop" : ""}
     />
-  </Fragment >
+  </Fragment>
 );
 
 export default Sidebar;
