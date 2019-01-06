6da731b01f543304304b8476aa41f519d84ef2fc
Florentin C <florentin.coyard@gmail.com>
2019-01-06 14:21:41 +0100

    Made the redirection if not logged

--- front_backpackers/src/routes/index.js
+++ front_backpackers/src/routes/index.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Route, Switch } from "react-router";
+import React, { Component, Fragment } from "react";
+import { Route, Switch, Redirect } from "react-router";
 import { Container } from "reactstrap";
 import HomePageContainer from "../containers/HomepageContainer";
 import LoginPage from "../LoginPage/LoginPage";
@@ -9,18 +9,31 @@ import ActivityContainer from "../containers/ActivityContainer";
 import SignUpPage from "../SignUpPage/SignUpPage";
 import PlaceContainer from "../containers/PlaceContainer";
 
-const routes = (
-  <Container fluid>
-    <Switch>
-      <Route exact path="/" component={HomePageContainer} />
-      <Route path="/login" component={LoginPage} />
-      <Route path="/place/:id/newactivity" component={CreateActivityPage} />
-      <Route path="/profil" component={ProfileContainer} />
-      <Route path="/activity/:id" component={ActivityContainer} />
-      <Route path="/signup" component={SignUpPage} />
-      <Route path="/place/:id" component={PlaceContainer} />
-    </Switch>
-  </Container>
-);
+class Routes extends Component {
+  render() {
+    return (
+      <Container fluid>
+        <Switch>
+          <Route path="/login" component={LoginPage} />
+          <Route path="/signup" component={SignUpPage} />
+          <Route exact path="/" component={HomePageContainer} />
+          <Route path="/activity/:id" component={ActivityContainer} />
+          <Route path="/place/:id" component={PlaceContainer} />
+          {!localStorage.getItem('BackpackersToken') ?
+            <Fragment>
+              <Redirect from="/place/:id/newactivity" to="/login" />
+              <Redirect from="/profil" to="/login" />
+            </Fragment>
+            :
+            <Fragment>
+              <Route path="/place/:id/newactivity" component={CreateActivityPage} />
+              <Route path="/profil" component={ProfileContainer} />
+            </Fragment>
+          }
+        </Switch>
+      </Container>
+    )
+  }
+}
 
-export default routes;
+export default Routes;
