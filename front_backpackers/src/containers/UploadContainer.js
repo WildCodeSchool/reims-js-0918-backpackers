f16c9f01926971b7f6572bc6c2d7bbc37c637c4f
Michaël BOURMEU <michael.bourmeau@outlook.fr>
2019-01-08 10:03:21 +0100

    uploadContainer

---
+++ front_backpackers/src/containers/UploadContainer.js
@@ -0,0 +1,17 @@
+import { connect } from "react-redux";
+import { makeAFetchIdActivityAction } from "../actions/actions";
+import UploadFiles from "../FormPage/UploadFiles";
+import CreateActivityPage from "../FormPage/CreateActivityPage";
+
+const mapStateToProps = state => ({
+  idCurrent: state.idCurrent
+});
+
+const mapDispatchToProps = dispatch => ({
+  idCurrent: idCurrent => dispatch(makeAFetchIdActivityAction(idCurrent))
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(UploadFiles, CreateActivityPage);
