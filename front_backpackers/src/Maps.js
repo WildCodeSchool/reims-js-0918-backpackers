f1e8fbbec7647468977dba59b7268afe98761421
Florentin C <florentin.coyard@gmail.com>
2019-01-19 21:58:51 +0100

    clustermaker is now activated on the main maps

--- front_backpackers/src/Maps.js
+++ front_backpackers/src/Maps.js
@@ -2,17 +2,19 @@ import React, { Component, Fragment } from "react";
 import { Map, TileLayer, Marker } from "react-leaflet";
 import { Row } from "reactstrap";
 
-import L from "leaflet"
+import L from "leaflet";
+import MarkerClusterGroup from "react-leaflet-markercluster";
+import PlaceThumbnail from "./HomePage/PlaceThumbnail";
 
 import "./Maps.scss";
-import PlaceThumbnail from "./HomePage/PlaceThumbnail";
+require("react-leaflet-markercluster/dist/styles.min.css");
 
 const MyPopupMarker = ({ content, position, getMarkerInfos }) =>
   content ? (
     <Marker onClick={() => getMarkerInfos(content)} position={position} />
   ) : (
-      <Marker position={position} />
-    );
+    <Marker position={position} />
+  );
 
 const MyMarkersList = ({ markers, getMarkerInfos }) => {
   const items = markers.map(({ key, ...props }) => (
@@ -37,9 +39,13 @@ class Maps extends Component {
 
     var myIcon = L.icon({
       className: "myMarker",
-      iconUrl: `http://localhost:3010/images/${this.props.profile[0].picture ? this.props.profile[0].picture : "default.png"}`,
+      iconUrl: `http://localhost:3010/images/${
+        this.props.profile[0].picture
+          ? this.props.profile[0].picture
+          : "default.png"
+      }`,
       iconSize: [30, 30],
-      shadowUrl: '/images/marker_shadow.png',
+      shadowUrl: "/images/marker_shadow.png",
       shadowSize: [60, 50]
     });
     L.marker(this.props.map, { icon: myIcon }).addTo(map);
@@ -60,32 +66,36 @@ class Maps extends Component {
       content: place
     }));
 
-
     return (
       <Row>
         <Map
-          ref={m => { this.leafletMap = m; }}
+          ref={m => {
+            this.leafletMap = m;
+          }}
           maxBounds={[[-90, -180], [90, 180]]}
           center={this.props.map}
           zoom="15"
+          maxZoom="20"
           onClick={() => this.resetMarkerInfos()}
         >
           <TileLayer
             attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          <MyMarkersList
-            markers={markers}
-            getMarkerInfos={this.getMarkerInfos}
-          />
+          <MarkerClusterGroup>
+            <MyMarkersList
+              markers={markers}
+              getMarkerInfos={this.getMarkerInfos}
+            />
+          </MarkerClusterGroup>
         </Map>
         {this.state.markerInfos.name ? (
           <div className="mapPopup">
             <PlaceThumbnail {...this.state.markerInfos} />
           </div>
         ) : (
-            ""
-          )}
+          ""
+        )}
       </Row>
     );
   }
