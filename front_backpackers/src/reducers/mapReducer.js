583fedd522e7577078e9ceeb7fc9acdd78e42461
Florentin C <florentin.coyard@gmail.com>
2019-01-20 13:11:27 +0100

    Added reducer for map filter

--- front_backpackers/src/reducers/mapReducer.js
+++ front_backpackers/src/reducers/mapReducer.js
@@ -1,4 +1,4 @@
-import { GET_GEOLOCATION } from "../actions/actionTypes";
+import { GET_GEOLOCATION, GET_MAP_FILTER } from "../actions/actionTypes";
 
 const mapReducer = (
   previousState = { coords: [], filter: "Pas de filtre" },
@@ -7,6 +7,8 @@ const mapReducer = (
   switch (action.type) {
     case GET_GEOLOCATION:
       return { ...previousState, coords: action.coords };
+    case GET_MAP_FILTER:
+      return { ...previousState, filter: action.filter };
     default:
       return previousState;
   }
--- front_backpackers/src/reducers/mapReducer.test.js
+++ front_backpackers/src/reducers/mapReducer.test.js
@@ -1,5 +1,8 @@
 import mapReducer from "./mapReducer";
-import { makeGetGeolocationAction } from "../actions/actions";
+import {
+  makeGetGeolocationAction,
+  makeGetMapFilterAction
+} from "../actions/actions";
 
 describe("mapReducer", () => {
   it("handles GET_GEOLOCATION", () => {
@@ -7,6 +10,17 @@ describe("mapReducer", () => {
     const coords = [50.34, 0.234];
     const expected = { filter: "Pas de filtre", coords: [50.34, 0.234] };
     expect(mapReducer(fakeState, makeGetGeolocationAction(coords))).toEqual(
+      expected
+    );
+  });
+});
+
+describe("mapReducer", () => {
+  it("handles GET_MAP_FILTER", () => {
+    const fakeState = { coords: [], filter: "Pas de filtre" };
+    const filter = "bien-etre";
+    const expected = { filter: "bien-etre", coords: [] };
+    expect(mapReducer(fakeState, makeGetMapFilterAction(filter))).toEqual(
       expected
     );
   });
